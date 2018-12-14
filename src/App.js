2b0555b612df4a341ae7580c794acfb90aeb59e8
Dominick Painter <dominick.painter@clock.co.uk>
2018-12-15 10:53:08 +1100

    > Grid

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@ import Hero from './components/hero/Hero'
 import ProjectList from './components/project-list/ProjectList'
 import ProjectDetail from './components/project-detail/ProjectDetail'
 import Footer from './components/footer/Footer'
+import { Grid, Row, Col } from 'react-flexbox-grid';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <Navigation />
-        </header>
-        <Hero />
-        <ProjectList />
-        <ProjectDetail />
-        <Footer />
-      </div>
+      <Grid className="App" fluid>
+        <Row>
+          <Col xs={12}>
+            <header className="App-header">
+              <Navigation />
+            </header>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Hero />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={4}>
+            <ProjectList />
+          </Col>
+          <Col xs={12} md={8}>
+            <ProjectDetail />
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12}>
+            <Footer />
+          </Col>
+        </Row>
+      </Grid>
     )
   }
 }
