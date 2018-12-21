188df12333d61828aedd8bdbfdac7cdba8753ae0
Dominick Painter <dominick.painter@clock.co.uk>
2018-12-21 16:14:10 +1100

    > Screenshot

--- src/components/projects/project-detail/ProjectDetail.js
+++ src/components/projects/project-detail/ProjectDetail.js
@@ -1,9 +1,23 @@
 import React, { Fragment } from 'react'
+import AustralianScreen from '../../../images/screen-australian.png'
+import TigersScreen from '../../../images/screen-leicester.png'
+import RiotScreen from '../../../images/screen-riot.png'
+import SaintsScreen from '../../../images/screen-saints.png'
 
 const ProjectDetail = ({ activeProject = {} }) => {
+  const screenshots = {
+    'The Australian': AustralianScreen,
+    'Northampton Saints': SaintsScreen,
+    'Leicester Tigers': TigersScreen,
+    'Riot Games': RiotScreen
+  }
+
+  const screenshot = screenshots[activeProject.name]
+
   return (
     <Fragment>
       <h2 className="ProjectHeading">{activeProject.name}</h2>
+      {screenshot && <img className="ProjectScreenshot" src={screenshot} alt={activeProject.name} />}
       <div className="ProjectDetail" dangerouslySetInnerHTML={{__html: activeProject.detail }} />
     </Fragment>
   )
